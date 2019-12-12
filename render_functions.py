1b7417c8cfd936d3caa0e19bb8861aa175a0242d
SpookySnee <spookestcat@hotmail.com>
2019-12-12 14:03:22 -0800

    Render only on movement issue

--- render_functions.py
+++ render_functions.py
@@ -60,7 +60,7 @@ def render_all(con, panel, entities, player, game_map, fov_map, fov_recompute, m
 
             y = 1
             for message in message_log.messages:
-                libtcod.console_set_default_foreground(panel, message.color)
+                libtcod.console_set_default_foreground(panel, libtcod.white)
                 libtcod.console_print_ex(panel, message_log.x, y, libtcod.BKGND_NONE, libtcod.LEFT, message.text)
                 y += 1
 
